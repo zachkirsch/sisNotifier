2fb6dfef6b17a9de26dd0d43dfb115b0f00cdc92
Zach Kirsch <zachkirsch@gmail.com>
2016-12-26 02:58:56 -0500

    argument validation

--- notify.sh
+++ notify.sh
@@ -7,6 +7,21 @@
 # Argument 2: file containing SIS password
 # Argument 2: email to contact if the grades have been changed
 
+usage() {
+        echo "Usage: $0 <SIS username> <password file> <email>"
+}
+
+# check arguments
+if [[ $# -ne 3 ]]; then
+        echo "Wrong number of arguments."
+        usage
+        exit 1
+elif [[ ! -f "$2" ]]; then
+        echo "Password file does not exist."
+        usage
+        exit 1
+fi
+
 username="$1"
 password="$2"
 email="$3"
@@ -40,8 +55,8 @@ diff $old $new > /dev/null
 if [ $? -ne 0 ]; then # if diff had ouptut
         if [[ "$already_exists" = true ]]; then
                 mail -s "Grades updated" $email < $new
+                echo "*** Grades updated ***"
         fi
-        echo "*** Grades updated ***"
 fi
 
 mv $new $old
